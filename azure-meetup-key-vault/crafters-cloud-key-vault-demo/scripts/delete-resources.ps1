821af16fc7b2e1a68ccc60eacf0449895a98f260
Srdjan Zivojinovic <srdjan.zivojinovic@crafters.cloud>
2020-06-14 19:55:52 +0200

    Uncomment resource deletion.

--- azure-meetup-key-vault/crafters-cloud-key-vault-demo/scripts/delete-resources.ps1
+++ azure-meetup-key-vault/crafters-cloud-key-vault-demo/scripts/delete-resources.ps1
@@ -9,13 +9,13 @@ $config = Read-Configuration
 az account show
 
 Write-Host "Deleting web app: $($config.webAppName)"
-#az webapp delete --name $config.webAppName --resource-group $config.resourceGroup
+az webapp delete --name $config.webAppName --resource-group $config.resourceGroup
 
 Write-Host "Deleting app insights"
-#az resource delete --resource-group $config.resourceGroup --resource-type microsoft.insights/components --name $config.webAppName 
+az resource delete --resource-group $config.resourceGroup --resource-type microsoft.insights/components --name $config.webAppName 
 
 Write-Host "Soft deleting keyvault: $($config.keyVault)"
-#az keyvault delete --name $config.keyVault --resource-group $config.resourceGroup
+az keyvault delete --name $config.keyVault --resource-group $config.resourceGroup
 
 Write-Host "Purging keyvault: $($config.keyVault)"
 az keyvault purge --name $config.keyVault
